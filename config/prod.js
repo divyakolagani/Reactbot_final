85bbc1c7508dcec17e347baa3b1595b3f9098c59
divyakolagani <divyakolagani@gmail.com>
2020-12-15 12:42:54 +0530

    test11

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,8 @@
-const { json } = require("body-parser");
 
 module.exports = {
     projectId:process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionId:process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowLanguageCode:process.env.DIALOGFLOW_LANGUAGE_CODE,
     clientEmail:process.env.GOOGLE_CLIENT_EMAIL,
-    privateKey:json.parse(process.env.GOOGLE_PRIVATE_KEY)
+    privateKey:JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 }
