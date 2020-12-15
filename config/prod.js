f0a7a96774a48ae4824523f52043c9e3b2939fec
divyakolagani <divyakolagani@gmail.com>
2020-12-15 09:34:02 +0530

    modified project id in prod.js  file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ProjectId:process.env.GOOGLE_PROJECT_ID,
+    projectId:process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionId:process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowLanguageCode:process.env.DIALOGFLOW_LANGUAGE_CODE,
     clientEmail:process.env.GOOGLE_CLIENT_EMAIL,
