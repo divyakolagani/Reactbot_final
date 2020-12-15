f3dc1b21f2e1fb6ed8e2c7f3f6a7e7da20c9b589
divyakolagani <divyakolagani@gmail.com>
2020-12-15 11:38:12 +0530

    test5

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 if(process.env.NODE_ENV === 'production') {
-    console.log('if block')
     module.exports = require('./prod.js');
 } else {
-    console.log('else')
     module.exports = require('./dev.js'); } 
