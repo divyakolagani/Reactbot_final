9a96e35ac4e028ecbc6ff34fdda2119567ffa384
divyakolagani <divyakolagani@gmail.com>
2021-01-10 00:37:52 +0530

    included path library to package.json

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ require('./routes/dialogflowRoutes')(app);
 require('./routes/fulfillmentRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
